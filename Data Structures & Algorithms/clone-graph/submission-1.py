36e4cfcf33eb9f91e3a61e598823d0c5755d2399
Ryan Yavari <ryanyavari10@gmail.com>
2026-07-26 01:43:26 -0700

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.py
@@ -0,0 +1,43 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+
+'''
+
+populate a hasmap oldToNew that holds the new nodes as values and the old nodes as keys
+    node : newNode
+
+
+
+'''
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+
+        oldToNew = {}
+
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            
+            return copy
+        
+        return dfs(node) if node else None
+
+            
+
+
+
+        
