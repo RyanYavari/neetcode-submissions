cbe296328260cec9d40dfc48fb1d27beeb7874a9
Ryan Yavari <ryanyavari10@gmail.com>
2026-07-26 22:55:54 -0700

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.py
@@ -0,0 +1,72 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        '''
+
+        DFS
+
+        find height of left subtree and right subtree. if height difference is less than 2, tree is balanced. 
+
+        O(n) approach with dfs:
+        
+        dfs(node):
+            if not root: #root is a leaf, return true and height of 0
+
+            #if its node, find right and left subtree height to see if tree is balanced
+
+                if either left or right subtree is not balanced (false) OR  their height difference is mroe than one, return false. else return true
+        
+
+        '''
+
+        #DFS
+
+        def dfs(root):
+            #if root is Null -> Tree is balanced and has a height of 0
+            if not root:
+                return [True, 0]
+            
+            # if root exists, determine if its balanced by searching its right and left child.
+
+            right = dfs(root.right)
+            left = dfs(root.left)
+
+            # check if balanced
+
+            balanced = right[0] and left[0] and abs(right[1]-left[1]) < 2
+
+            #calculate height. 
+            height = 1 + max(right[1], left[1])
+
+            return [balanced, height]
+        
+
+        return dfs(root)[0]
+            
+
+
+         
+
+
+            
+
+            
+
+
+
+
+        
+
+
+
+        
+
+
+
